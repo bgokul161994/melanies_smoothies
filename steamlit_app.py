42ff55631491f6a4d96918120df89732ff704298
bgokul161994 <60784664+bgokul161994@users.noreply.github.com>
2024-09-11 07:40:52 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,60 +1,23 @@
-##########################  Add a Submit Button##########################
+####################################Add a Name Box for Smoothie Orders##################################
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
-# Write directly to the app
-st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
-st.write(
-"""Choose the fruits you want in your custom Smoothie!
-""")
 from snowflake.snowpark.functions import col
-session = get_active_session()
-my_dataframe = session.table('smoothies.public.fruit_options').select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-ingredients_list = st.multiselect(
-'Choose up to 5 ingredients:'
-, my_dataframe
-,max_selections= 5
+
+# Write directly to the app
+st.title(":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
+st.write(
+        """Choose the fruits you want in your smoothie!
+        """
 )
-if ingredients_list:
-    ingredients_string = ''
-
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-
-    #st.write (ingredients_string)
-
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
-
-    #st.write(my_insert_stmt)
-    time_to_insert = st.button ('Submit order')
-
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-    
-        st.success('Your Smoothie is ordered!', icon="✅")
 
 
+session = get_active_session()
+my_dataframe = session.table('smoothies.public.orders').select(col('ingredients'),col('name_on_order'),col('ORDER_FILLED'))
+#my_dataframe = session.table('smoothies.public.fruit_options').select(col('FRUIT_NAME'))
+editable_df = st.data_editor(my_dataframe)
 
+submitted = st.button ('submit')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if submitted:
+    st.success("Someone clicked the button.", icon ="✅" )

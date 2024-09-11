1cd8fe856a67d7663d1ff31c2d50f1f5f19a86d6
bgokul161994 <60784664+bgokul161994@users.noreply.github.com>
2024-09-11 08:35:54 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -22,18 +22,15 @@ submitted = st.button ('submit')
 if submitted:
     st.success("Someone clicked the button.", icon ="✅" )
 
-
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response).json())
-fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
-
-
 if ingredients_list:
         ingredients_string = ''
 
         for fruit_chosen in ingredients_list:
                 ingredients_string += fruit_chosen + ' '
-                fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+                st.subheader (fruit_chosen + 'Nutrition Information')
+                fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon + fruit_chosen")
                 fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width
+
+import requests
+
 

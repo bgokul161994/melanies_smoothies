fa20366918ebbe5fedbff3666dd80f30f2a53867
bgokul161994 <60784664+bgokul161994@users.noreply.github.com>
2024-09-11 16:51:13 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -27,6 +27,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
         
 #st.write(ingredients_string)
     
@@ -42,9 +44,7 @@ if ingredients_list:
         #session.sql(my_insert_stmt).collect()
         #st.success("your smoothie is ordered")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
 
 
 #if ingredients_list:

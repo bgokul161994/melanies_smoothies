17c055d55765e07787e2c60941ba32a34354d42d
bgokul161994 <60784664+bgokul161994@users.noreply.github.com>
2024-09-11 16:44:51 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -44,19 +44,19 @@ if ingredients_list:
 
         #st.success("your smoothie is ordered")
 
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response)
-#fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
-if ingredients_list:
-        ingredients_string = ''
-
-        for fruit_chosen in ingredients_list:
-                ingredients_string += fruit_chosen + ' '
-                st.subheader(fruit_chosen + 'Nutrition Information')
-                fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
-                fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+#if ingredients_list:
+ #       ingredients_string = ''
+#
+ #       for fruit_chosen in ingredients_list:
+  #              ingredients_string += fruit_chosen + ' '
+   #             st.subheader(fruit_chosen + 'Nutrition Information')
+    #            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
+     #           fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
 

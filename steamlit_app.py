a187e0d2975c68bb276e926f36f5fba6cd3607fb
bgokul161994 <60784664+bgokul161994@users.noreply.github.com>
2024-09-11 16:42:55 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -46,17 +46,17 @@ if ingredients_list:
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #st.text(fruityvice_response)
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+#fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
-#if ingredients_list:
-#        ingredients_string = ''
-#
- #       for fruit_chosen in ingredients_list:
-  #              ingredients_string += fruit_chosen + ' '
-   #             st.subheader(fruit_chosen + 'Nutrition Information')
-    #            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
-     #           fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+if ingredients_list:
+        ingredients_string = ''
+
+        for fruit_chosen in ingredients_list:
+                ingredients_string += fruit_chosen + ' '
+                st.subheader(fruit_chosen + 'Nutrition Information')
+                fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+                fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
 

76d9f93f9e7578b8d3b35d7eb0b041fa014c6a17
bgokul161994 <60784664+bgokul161994@users.noreply.github.com>
2024-09-11 16:41:41 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -45,7 +45,8 @@ if ingredients_list:
         #st.success("your smoothie is ordered")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+#st.text(fruityvice_response)
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
 #if ingredients_list:

22ceab5821e356f824e0409dab909b85f4132f61
bgokul161994 <60784664+bgokul161994@users.noreply.github.com>
2024-09-11 16:40:46 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -37,10 +37,10 @@ if ingredients_list:
     #st.write(my_insert_stmt)
     #st.stop()
 
-    time_to_insert = st.button ('Submit order')
+    #time_to_insert = st.button ('Submit order')
 
-    if time_to_insert :
-        session.sql(my_insert_stmt).collect()
+    #if time_to_insert :
+        #session.sql(my_insert_stmt).collect()
 
         #st.success("your smoothie is ordered")
 
